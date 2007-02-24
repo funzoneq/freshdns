8281bbf920b28d58666aad91f1967da0e14e705e
funzoneq2 <funzoneq2@b5c0238a-5327-0410-984c-81b30cfb2828>
2007-02-24 18:52:10 +0000

    new logo's. Added FreshDNS project page link to FreshDNS installations
    git-svn-id: https://freshdns.googlecode.com/svn@10 b5c0238a-5327-0410-984c-81b30cfb2828

--- trunk/templates/footer.tpl.php
+++ trunk/templates/footer.tpl.php
@@ -1,8 +1,9 @@
 </div>
 
 <div id="footer"><p><a href="http://jigsaw.w3.org/css-validator/check/referer/#huh">
-<img src="./images/css2-small.png" width="80" height="15" alt="We Try To Make CSS2 Stylesheets" /></a> <a href="http://validator.w3.org/check?uri=referer">
-<img src="./images/xhtml10-small.png" width="80" height="15" alt="XHTML Rulez" /></a></p></div>
+<img src="./images/css2.png" width="80" height="15" alt="We Try To Make CSS2 Stylesheets" /></a> <a href="http://validator.w3.org/check?uri=referer">
+<img src="./images/xhtml.png" width="80" height="15" alt="XHTML Rulez" /></a> <a href="http://code.google.com/p/freshdns/">
+<img src="./images/freshdns.png" width="80" height="15" alt="FreshDNS AJAX based, PowerDNS administration system" /></a></p></div>
 
 </body>
 </html>
