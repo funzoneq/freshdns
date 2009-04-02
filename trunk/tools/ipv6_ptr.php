83b13b6b3c96db9d4f82a48b63097bef7fb3eca4
funzoneq2 <funzoneq2@b5c0238a-5327-0410-984c-81b30cfb2828>
2009-04-02 20:20:31 +0000

    IPv6 PTR generator
    git-svn-id: https://freshdns.googlecode.com/svn@18 b5c0238a-5327-0410-984c-81b30cfb2828

--- trunk/tools/ipv6_ptr.php
+++ trunk/tools/ipv6_ptr.php
@@ -1,27 +1,56 @@
 <?php
-$stdptr = 'hosted.by.example.com';
-$iprange = '127.0.0.0'; // we anticipate a /24
+$prefix = "2001:888:2000:0013::/64";	// You're assigned IPv6 prefix
+$stdptr = 'ipv6.example.com';			// The default FQDN we return when reverse DNS is looked up
 
-$config['DNS']['templates']['ipv4ptr'] = array( # DO NOT CHANGE, UNLESS YOU KNOW WHAT YOU ARE DOING!
+$config['DNS']['templates']['ipv6ptr'] = array( # DO NOT CHANGE, UNLESS YOU KNOW WHAT YOU ARE DOING!
 array("name" => "{#DOMAIN#}",		"type" => "SOA",	"content" => "{#NS0#} {#HOSTMASTER#} {#SOACODE#}",	"prio" => "0",	"ttl" => "3600"),
 array("name" => "{#DOMAIN#}",		"type" => "NS",		"content" => "{#NS0#}",								"prio" => "0",	"ttl" => "3600"),
 array("name" => "{#DOMAIN#}",		"type" => "NS",		"content" => "{#NS1#}",								"prio" => "0",	"ttl" => "3600"),
-array("name" => "{#DOMAIN#}",		"type" => "NS",		"content" => "{#NS2#}",								"prio" => "0",	"ttl" => "3600"));
+array("name" => "{#DOMAIN#}",		"type" => "NS",		"content" => "{#NS2#}",								"prio" => "0",	"ttl" => "3600"),
+array("name" => "*.{#DOMAIN#}",		"type" => "PTR",	"content" => "{#STDPTR#}",							"prio" => "0",  "ttl" => "3600"));
 
 /* DO NOT EDIT BELOW THIS LINE! */
 include_once("../config.inc.php");
 
 $manager = new Manager($config['database']);
 
-$arpa = array_reverse(explode(".", $iprange));
-unset($arpa[0]);
-$name = implode(".", $arpa);
+function generateIPv6ARPA ($prefix)
+{
+	$prefix = explode("::", $prefix);
+	$prefix = $prefix[0];
+
+	$parts = explode(":", $prefix);
+
+	foreach($parts AS $key => $part)
+	{
+		while(strlen($part) < 4)
+		{
+			$part = '0'.$part;
+		}
+	
+		$parts[$key] = $part;
+	}
+
+	$reverse = implode("", $parts);
+
+	$arpa = "ip6.arpa";
+
+	for($i=0; $i<strlen($reverse); $i++)
+	{
+		$char = substr($reverse, $i, 1);
+		$arpa = $char.'.'.$arpa;
+	}
+
+	return $arpa;
+}
+
+$name = generateIPv6ARPA($prefix);
 
 try {
 	$domainId = $manager->addDomain ($name, '', 0, 'NATIVE', 0, '');
 	$manager->addZone($domainId, 1, "");
 	
-	foreach($config['DNS']['templates']['ipv4ptr'] AS $record)
+	foreach($config['DNS']['templates']['ipv6ptr'] AS $record)
 	{
 		$record['name']		= str_replace("{#DOMAIN#}",		$_POST['domain'],				$record['name']);
 		$record['content']	= str_replace("{#DOMAIN#}",		$_POST['domain'],				$record['content']);
@@ -33,11 +62,6 @@ try {
 		$record['content']	= str_replace("{#STDPTR#}",		$stdptr,						$record['content']);
 		
 		$manager->addRecord ($domainId, $record['name'], $record['type'], $record['content'], $record['ttl'], $record['prio'], $record['changeDate']);
-	}
-
-	for($i=0; $i<256; $i++)
-	{
-		$manager->addRecord($domainId, $i.'.'.$name, 'PTR', $stdptr, 3600, 0, 0);
 	}
 }catch (Exception $ex)
 {
