33ed0f501b0f382bfc3f7a9684041e61a46a810e
Max <max@teamwiki.de>
2015-05-31 15:20:42 +0200

    change design

--- templates/footer.tpl.php
+++ templates/footer.tpl.php
@@ -1,4 +1,6 @@
 </div>
+</div></div>
+</div>
 
 <div id="footer"><p><a href="http://jigsaw.w3.org/css-validator/check/referer/#huh">
 <img src="./images/css2.png" width="80" height="15" alt="We Try To Make CSS2 Stylesheets" /></a> <a href="http://validator.w3.org/check?uri=referer">
--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -14,42 +14,64 @@
 	echo "var myUserId='".$_SESSION['userId']."';\n";
 	?>
 	</script>
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="./images/style.css" type="text/css" />
 </head>
 
 <body>
 
-<div id="query"></query>
+<div id="query"></div>
 
-<div id="header">
-	<div id="logo"><img src="./images/logo.png" alt="FreshDNS" /></div>
-	
-	<div id="headingtext">
-		<div id="title">FreshDNS</div>
-	</div>
-</div>
+<div id="header" class="navbar navbar-static-top navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#" onclick="list('');return false;"><img id="logo" src="./images/logo.png" alt="FreshDNS" /> FreshDNS</a>
+    </div>
 
-<div id="navbar">
-	<div id="letters">
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
 	<?php if($login->isLoggedIn()){ ?>
-	<a href="javascript:list('[0-9]');">0-9</a> <a href="javascript:list('a');">A</a> <a href="javascript:list('b');">B</a> <a href="javascript:list('c');">C</a>
-	<a href="javascript:list('d');">D</a> <a href="javascript:list('e');">E</a> <a href="javascript:list('f');">F</a> <a href="javascript:list('g');">G</a> <a href="javascript:list('h');">H</a>
-	<a href="javascript:list('i');">I</a> <a href="javascript:list('j');">J</a> <a href="javascript:list('k');">K</a> <a href="javascript:list('l');">L</a> <a href="javascript:list('m');">M</a>
-	<a href="javascript:list('n');">N</a> <a href="javascript:list('o');">O</a> <a href="javascript:list('p');">P</a> <a href="javascript:list('q');">Q</a> <a href="javascript:list('r');">R</a>
-	<a href="javascript:list('s');">S</a> <a href="javascript:list('t');">T</a> <a href="javascript:list('u');">U</a> <a href="javascript:list('v');">V</a> <a href="javascript:list('w');">W</a>
-	<a href="javascript:list('x');">X</a> <a href="javascript:list('y');">Y</a> <a href="javascript:list('z');">Z</a> <a href="javascript:newDomain();">NEW</a> <a href="javascript:bulkNewDomain();">BULK</a>
+	<li><a href="javascript:newDomain();">NEW</a></li>
+	<li><a href="javascript:bulkNewDomain();">BULK</a></li>
 	<?php if($_SESSION['level'] < 10) { ?>
-	<a href="javascript:editUser(<?php echo $_SESSION['userId']; ?>);">PROFILE</a> 
+	<li><a href="javascript:editUser(<?php echo $_SESSION['userId']; ?>);">PROFILE</a> </li>
 	<?php }else{ ?>
-	<a href="javascript:userAdmin();">USERS</a> 
+	<li><a href="javascript:userAdmin();">USERS</a> </li>
 	<?php } ?>
-	<a href="index.php?p=logout">LOGOUT</a>
-	<?php }else{ echo "&nbsp;"; } ?></div>
-	<div id="search">
+	<li><a href="index.php?p=logout">LOGOUT</a></li>
+	<?php }else{ echo "&nbsp;"; } ?>
+
+
+      </ul>
+
 	<?php if($login->isLoggedIn()){ ?>
-	<form style="margin:0px;" name="searchform" method="get" action="index.php" id="searchform">
-	<input id="livesearch" name="q" type="text" onkeypress="liveSearchStart()" value="<?php echo $_GET['q']; ?>"></form>
-	<?php } ?></div>
+	<form name="searchform" method="get" action="index.php" id="searchform" class="navbar-form navbar-right" role="search">
+        <div class="form-group">
+	<input id="livesearch" name="q" type="text" onkeypress="liveSearchStart()" value="<?php echo $_GET['q']; ?>" class="form-control" placeholder="Search">
+	</div></form>
+	<?php } ?>
+
+    </div>
+
+  </div>
 </div>
+
+<div class="container">
+<div class="row"><div class="col-sm-12">
+	<ul class="pagination">
+	<?php if($login->isLoggedIn()){ ?>
+	<li><a href="javascript:list('[0-9]');">0-9</a></li>
+	<?php for($i=0x41;$i<=0x40+26; $i++) { ?> <li><a href="javascript:list('<?=chr($i)?>');"><?= chr($i)?></a></li><?php } ?>
+	<?php } ?>
+	</ul>
 
 <div id="body">
+
